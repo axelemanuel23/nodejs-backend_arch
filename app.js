ec4f1629ee8dc56339924682f308f96b53862e2d
Lex A <axelemanuelpavon@gmail.com>
2023-03-11 03:57:32 -0300

    agregando una variable de entorno

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
-const whitelist = ["http://localhost:3000", "https://tecnomix-backend-production.up.railway.app/"];
+const whitelist = ["http://localhost:3000", process.env.CLIENTSIDE];
 
 app.use(cors({origin: whitelist, methods: ["GET", "POST", "PATCH", "DELETE"]}));
 
