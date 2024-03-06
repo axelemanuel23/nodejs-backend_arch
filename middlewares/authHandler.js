73007a8bcceca39482b251301ec9b10df8c81a69
Lex A <axelemanuelpavon@gmail.com>
2024-03-06 02:01:06 -0300

    update

--- middlewares/authHandler.js
+++ middlewares/authHandler.js
@@ -3,7 +3,7 @@ const boom = require("@hapi/boom");
 async function authorizationHandler(req, res, next){
     try{
         if(req.headers.apikey!=process.env.APIKEY){
-            throw boom.unauthorized("Auth Failed");
+            throw boom.unauthorized("Auth Failed by APIKEY");
         }
         next();
     }catch(err){
